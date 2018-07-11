4929cf32fe4b4c65a2cf2eaa95046fca530dc9d0
Jameel Briones <jbriones@redhat.com>
2018-07-11 15:09:54 +0100

    separate username input to a component

--- react-web/src/components/CreateSession.js
+++ react-web/src/components/CreateSession.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { Form, FormGroup, ControlLabel, FormControl, Button, HelpBlock } from 'patternfly-react';
+
+import { UsernameInput } from './UsernameInput';
 
 export class CreateSession extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pristine: true
-    }
-  }
-
   componentDidMount() {
     // get session id
     fetch(process.env.REACT_APP_API_HOST + "/session")
@@ -22,19 +15,8 @@ export class CreateSession extends React.Component {
     this.props.sessionStarted();
   }
 
-  handleInputChange = (event) => {
-    if (this.state.pristine) {
-      this.setState({pristine: false});
-    }
-    if (event.target.value && event.target.value.trim()) {
-      this.setState({userName: event.target.value});
-    } else {
-      this.setState({userName: null});
-    }
-  };
-
-  startSession = () => {
-    window.localStorage.setItem("userName", this.state.userName);
+  startSession = (userName) => {
+    window.localStorage.setItem("userName", userName);
     this.props.redirectToSession(this.state.sessionId);
   };
 
@@ -44,20 +26,7 @@ export class CreateSession extends React.Component {
         <header className="text-center">
           <h1> Start a New Storypoint Session </h1>
         </header>
-        <div>
-          <Form onSubmit={this.startSession}>
-            <FormGroup controlId="userName" validationState={!this.state.pristine && !this.state.userName ? 'error': 'success'}>
-              <ControlLabel>
-                Username:
-              </ControlLabel>
-              <FormControl type="text" onChange={this.handleInputChange} />
-              {(!this.state.userName && !this.state.pristine) && <HelpBlock> Username can't be empty </HelpBlock>}
-            </FormGroup>
-            <Button className="pull-right" bsStyle="primary" type="submit" value="Submit" disabled={this.state.userName && !this.state.pristine ? false : true}>
-              Start Session
-            </Button>
-          </Form>
-        </div>
+        <UsernameInput submit={this.startSession} btnContext='Start Session' />
       </div>
     );
   }
